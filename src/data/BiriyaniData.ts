aadb56a1a4261b30cb9d2014f232879c879a42b8
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2024-01-15 01:33:40 +0530

    added biriyaniData

---
+++ src/data/BiriyaniData.ts
@@ -0,0 +1,55 @@
+const BiriyaniData = [
+  {
+    id: 'C1',
+    name: 'Regular Chicken Biriyani',
+    description:
+      'Biriyani made with Spices and Chicken, served along with an Egg and a Roast Chicken piece; Raita and Kurma on the Side. (Suffices for two people)',
+    roasted: 'Medium Roasted',
+    imagelink_square: require('../assets/biriyani_assets/biriyani/biriyani1.jpg'),
+    imagelink_portrait: require('../assets/biriyani_assets/biriyani/biriyani1.jpg'),
+    ingredients: 'chiken',
+    special_ingredient: 'With watalappan',
+    prices: [{size: 'F', price: '1500', currency: 'LKR '}],
+    average_rating: 4.7,
+    ratings_count: '6,879',
+    favourite: false,
+    type: 'biriyani',
+    index: 0,
+  },
+  {
+    id: 'C2',
+    name: 'Large Chicken Biriyani',
+    description:
+      'Biriyani made with Spices and Chicken, served along with two Eggs and two Roast Chicken pieces; Raita and Kurma on the Side. (Suffices for three people)',
+    roasted: 'Medium Roasted',
+    imagelink_square: require('../assets/biriyani_assets/biriyani_assets/biriyani1.jpg'),
+    imagelink_portrait: require('../assets/biriyani_assets/biriyani/biriyani1.jpg'),
+    ingredients: 'chiken',
+    special_ingredient: 'With Watalappam',
+    prices: [{size: 'F', price: '2500', currency: 'LKR '}],
+    average_rating: 4.8,
+    ratings_count: '6,900',
+    favourite: false,
+    type: 'biriyani',
+    index: 1,
+  },
+  {
+    id: 'C3',
+    name: 'Chicken Biriyani Sawan',
+    description:
+      'Biriyani made with Spices and Chicken, served along with Eggs and Roast Chicken pieces; Raita and Kurma on the Side.\n' +
+      '(Suffice for Seven People as it has seven eggs and seven roast chicken pieces)',
+    roasted: 'Medium Roasted',
+    imagelink_square: require('../assets/biriyani_assets/biriyani_assets/biriyani1.jpg'),
+    imagelink_portrait: require('../assets/biriyani_assets/biriyani/biriyani1.jpg'),
+    ingredients: 'Spices and Chicken',
+    special_ingredient: 'With Watalappam',
+    prices: [{size: 'F', price: '2500', currency: 'LKR'}],
+    average_rating: 4.8,
+    ratings_count: '6,900',
+    favourite: false,
+    type: 'biriyani',
+    index: 1,
+  },
+];
+export default BiriyaniData;
