121e732b12243fd3429ee8f0e030c126d469ddb8
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2024-01-16 16:46:37 +0530

    set homescreen categoriesData

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useStore } from '../store/store'
+
 
 const HomeScreen = () => {
+  const BiriyaniList = useStore((state:any) => state.BiriyaniList);
+  const DrinksList = useStore((state:any) => state.DrinksList);
+ const [catagories,setCategories] = useState(undefined)
+
   return (
     <View>
       <Text>HomeScreen</Text>
